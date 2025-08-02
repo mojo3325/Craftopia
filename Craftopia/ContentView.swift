69270adea29809b9dc787654bb8a5756653b713b
mojo3325 <ustenkoegorp100dl@gmail.com>
2025-08-02 19:33:10 +0200

    Initial Commit

---
+++ Craftopia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Craftopia
+//
+//  Created by Yehor Ustenko on 02.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Craftopia/CraftopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftopiaApp.swift
+//  Craftopia
+//
+//  Created by Yehor Ustenko on 02.08.25.
+//
+
+import SwiftUI
+
+@main
+struct CraftopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
